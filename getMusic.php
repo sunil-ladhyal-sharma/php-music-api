5f6242c78375e4ef013d078b9589168d98c677de
Sunil Sharma <sunil.ladhyal.sharma@gmail.com>
2022-10-05 17:47:17 +0530

    feat(angular-music-01): music api using php version 1

---
+++ getMusic.php
@@ -0,0 +1,25 @@
+<?php
+
+$arr = [
+    ['name'=> 'file (1)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (1).mp3'],
+    ['name'=> 'file (2)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (2).mp3'],
+    ['name'=> 'file (3)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (3).mp3'],
+    ['name'=> 'file (4)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (4).mp3'],
+    ['name'=> 'file (5)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (5).mp3'],
+    ['name'=> 'file (6)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (6).mp3'],
+    ['name'=> 'file (7)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (7).mp3'],
+    ['name'=> 'file (8)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (8).mp3'],
+    ['name'=> 'file (9)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (9).mp3'],
+    ['name'=> 'file (10)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (10).mp3'],
+    ['name'=> 'file (11)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (11).mp3'],
+    ['name'=> 'file (12)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (12).mp3'],
+    ['name'=> 'file (13)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (13).mp3'],
+    ['name'=> 'file (14)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (14).mp3'],
+    ['name'=> 'file (15)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (15).mp3'],
+    ['name'=> 'file (16)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (16).mp3'],
+    ['name'=> 'file (17)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (17).mp3'],
+    ['name'=> 'file (18)','description'=>'N/A','Artist'=>'N/A','path'=>'music/file (18).mp3']
+];
+
+echo json_encode($arr);
+?>
